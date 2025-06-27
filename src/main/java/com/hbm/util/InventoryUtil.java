cd8c407383b2824a66da1fd2c1352a8df0e17f62
mlbv <51232730+mlbv@users.noreply.github.com>
2025-06-28 02:27:06 +0800

    fix: fluid cells not working

--- src/main/java/com/hbm/util/InventoryUtil.java
+++ src/main/java/com/hbm/util/InventoryUtil.java
@@ -372,7 +372,7 @@ public class InventoryUtil {
 		ItemStack[] copy = ItemStackUtil.carefulCopyArrayTruncate(inv, start, end);
 
 		for (ItemStack item : items) {
-			if (item.isEmpty()) {
+			if (item == null || item.isEmpty()) {
 				continue;
 			}
 
