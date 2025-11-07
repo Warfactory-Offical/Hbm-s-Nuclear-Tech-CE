b55528ac893a7fc18bf7a0f5eb01fc48fdb446c3
MrNorwood <[email protected]>
2025-11-07 02:59:52 +0100

    vao

--- src/main/java/com/hbm/hfr/render/loader/WavefrontObjVBO.java
+++ src/main/java/com/hbm/hfr/render/loader/WavefrontObjVBO.java
@@ -3,74 +3,96 @@ package com.hbm.hfr.render.loader;
 import com.hbm.render.amlfrom1710.IModelCustom;
 import com.hbm.render.amlfrom1710.TextureCoordinate;
 import com.hbm.render.amlfrom1710.Vertex;
+import net.minecraft.client.renderer.GlStateManager;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11; import net.minecraft.client.renderer.GlStateManager;
-import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL30;
 
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+
 public class WavefrontObjVBO implements IModelCustom {
-    class VBOBufferData {
-
-        String name;
-        int vertices = 0;
-        int vertexHandle;
-        int uvHandle;
-        int normalHandle;
-
-    }
-
-    List<VBOBufferData> groups = new ArrayList<VBOBufferData>();
-
+    private static final int FLOAT_SIZE = 4;
+    private static final int STRIDE = 9 * FLOAT_SIZE;
     static int VERTEX_SIZE = 3;
-    static int UV_SIZE = 3;
-
+    List<VBOBufferData> groups = new ArrayList<>();
     public WavefrontObjVBO(HFRWavefrontObject obj) {
-        for(S_GroupObject g : obj.groupObjects) {
+        for (S_GroupObject g : obj.groupObjects) {
+
             VBOBufferData data = new VBOBufferData();
             data.name = g.name;
 
-            FloatBuffer vertexData = BufferUtils.createFloatBuffer(g.faces.size() * 3 * VERTEX_SIZE);
-            FloatBuffer uvData = BufferUtils.createFloatBuffer(g.faces.size() * 3 * UV_SIZE); //TODO: I think we can save some memory here
-            FloatBuffer normalData = BufferUtils.createFloatBuffer(g.faces.size() * 3 * VERTEX_SIZE);
+            List<Float> vertexData = new ArrayList<>(g.faces.size() * 3 * VERTEX_SIZE);
+            List<Float> uvwData = new ArrayList<>(g.faces.size() * 3 * VERTEX_SIZE);
+            List<Float> normalData = new ArrayList<>(g.faces.size() * 3 * VERTEX_SIZE);
 
-            for(S_Face face : g.faces) {
-                for(int i = 0; i < face.vertices.length; i++) {
+
+            for (S_Face face : g.faces) {
+                for (int i = 0; i < face.vertices.length; i++) {
                     Vertex vert = face.vertices[i];
                     TextureCoordinate tex = new TextureCoordinate(0, 0);
                     Vertex normal = face.vertexNormals[i];
 
-                    if(face.textureCoordinates != null && face.textureCoordinates.length > 0) {
+                    if (face.textureCoordinates != null && face.textureCoordinates.length > 0) {
                         tex = face.textureCoordinates[i];
                     }
 
                     data.vertices++;
-                    vertexData.put(new float[] { vert.x, vert.y, vert.z });
-                    uvData.put(new float[] { tex.u, tex.v, tex.w });
-                    normalData.put(new float[] { normal.x, normal.y, normal.z });
+                    vertexData.add(vert.x);
+                    vertexData.add(vert.y);
+                    vertexData.add(vert.z);
+
+                    uvwData.add(tex.u);
+                    uvwData.add(tex.v);
+                    uvwData.add(tex.w);
+
+                    normalData.add(normal.x);
+                    normalData.add(normal.y);
+                    normalData.add(normal.z);
                 }
             }
-            vertexData.flip();
-            uvData.flip();
-            normalData.flip();
+            float[] combinedData = new float[data.vertices * 9];
+            int dst = 0;
+            for (int i = 0; i < data.vertices; i++) {
+                combinedData[dst++] = vertexData.get(i * 3);
+                combinedData[dst++] = vertexData.get(i * 3 + 1);
+                combinedData[dst++] = vertexData.get(i * 3 + 2);
 
-            data.vertexHandle = GL15.glGenBuffers();
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, data.vertexHandle);
-            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertexData, GL15.GL_STATIC_DRAW);
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+                combinedData[dst++] = uvwData.get(i * 3);
+                combinedData[dst++] = uvwData.get(i * 3 + 1);
+                combinedData[dst++] = uvwData.get(i * 3 + 2);
 
-            data.uvHandle = GL15.glGenBuffers();
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, data.uvHandle);
-            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, uvData, GL15.GL_STATIC_DRAW);
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+                combinedData[dst++] = normalData.get(i * 3);
+                combinedData[dst++] = normalData.get(i * 3 + 1);
+                combinedData[dst++] = normalData.get(i * 3 + 2);
+            }
 
-            data.normalHandle = GL15.glGenBuffers();
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, data.normalHandle);
-            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, normalData, GL15.GL_STATIC_DRAW);
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
 
+            FloatBuffer buffer = BufferUtils.createFloatBuffer(combinedData.length);
+            buffer.put(combinedData);
+            buffer.flip();
+
+            data.vaoHandle = GL30.glGenVertexArrays();
+            data.vboHandle = glGenBuffers();
+            GL30.glBindVertexArray(data.vaoHandle);
+            glBindBuffer(GL_ARRAY_BUFFER, data.vboHandle);
+            glBufferData(GL_ARRAY_BUFFER, buffer, GL_STATIC_DRAW);
+
+            GL11.glVertexPointer(3, GL11.GL_FLOAT, STRIDE, 0L);
+            glEnableClientState(GL_VERTEX_ARRAY);
+
+            GL11.glTexCoordPointer(3, GL11.GL_FLOAT, STRIDE, 3L * Float.BYTES);
+            glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+            GL11.glNormalPointer(GL11.GL_FLOAT, STRIDE, 6L * Float.BYTES);
+            glEnableClientState(GL_NORMAL_ARRAY);
+
+            GL30.glBindVertexArray(0);
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
             groups.add(data);
         }
     }
@@ -81,40 +103,23 @@ public class WavefrontObjVBO implements IModelCustom {
     }
 
     private void renderVBO(VBOBufferData data) {
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, data.vertexHandle);
-        GL11.glVertexPointer(VERTEX_SIZE, GL11.GL_FLOAT, 0, 0l);
-
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, data.uvHandle);
-        GL11.glTexCoordPointer(UV_SIZE, GL11.GL_FLOAT, 0, 0l);
-
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, data.normalHandle);
-        GL11.glNormalPointer(GL11.GL_FLOAT, 0, 0l);
-
-        GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
-        GL11.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-        GL11.glEnableClientState(GL11.GL_NORMAL_ARRAY);
-
-        GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, data.vertices);
-
-        GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY);
-        GL11.glDisableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-        GL11.glDisableClientState(GL11.GL_NORMAL_ARRAY);
-
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        GL30.glBindVertexArray(data.vaoHandle);
+        GlStateManager.glDrawArrays(GL11.GL_TRIANGLES, 0, data.vertices);
+        GL30.glBindVertexArray(0);
     }
 
     @Override
     public void renderAll() {
-        for(VBOBufferData data : groups) {
+        for (VBOBufferData data : groups) {
             renderVBO(data);
         }
     }
 
     @Override
     public void renderOnly(String... groupNames) {
-        for(VBOBufferData data : groups) {
-            for(String name : groupNames) {
-                if(data.name.equalsIgnoreCase(name)) {
+        for (VBOBufferData data : groups) {
+            for (String name : groupNames) {
+                if (data.name.equalsIgnoreCase(name)) {
                     renderVBO(data);
                 }
             }
@@ -123,8 +128,8 @@ public class WavefrontObjVBO implements IModelCustom {
 
     @Override
     public void renderPart(String partName) {
-        for(VBOBufferData data : groups) {
-            if(data.name.equalsIgnoreCase(partName)) {
+        for (VBOBufferData data : groups) {
+            if (data.name.equalsIgnoreCase(partName)) {
                 renderVBO(data);
             }
         }
@@ -132,38 +137,45 @@ public class WavefrontObjVBO implements IModelCustom {
 
     @Override
     public void renderAllExcept(String... excludedGroupNames) {
-        for(VBOBufferData data : groups) {
+        for (VBOBufferData data : groups) {
             boolean skip = false;
-            for(String name : excludedGroupNames) {
-                if(data.name.equalsIgnoreCase(name)) {
+            for (String name : excludedGroupNames) {
+                if (data.name.equalsIgnoreCase(name)) {
                     skip = true;
                     break;
                 }
             }
-            if(!skip) {
+            if (!skip) {
                 renderVBO(data);
             }
         }
     }
 
     @Override
-    public void tessellateAll(com.hbm.render.amlfrom1710.Tessellator tes){
+    public void tessellateAll(com.hbm.render.amlfrom1710.Tessellator tes) {
         throw new RuntimeException("Tessellate not supported on HFR model");
     }
 
     @Override
-    public void tessellatePart(com.hbm.render.amlfrom1710.Tessellator tes, String name){
+    public void tessellatePart(com.hbm.render.amlfrom1710.Tessellator tes, String name) {
         throw new RuntimeException("Tessellate not supported on HFR model");
     }
 
     @Override
-    public void tessellateOnly(com.hbm.render.amlfrom1710.Tessellator tes, String... names){
+    public void tessellateOnly(com.hbm.render.amlfrom1710.Tessellator tes, String... names) {
         throw new RuntimeException("Tessellate not supported on HFR model");
     }
 
     @Override
-    public void tessellateAllExcept(com.hbm.render.amlfrom1710.Tessellator tes, String... excluded){
+    public void tessellateAllExcept(com.hbm.render.amlfrom1710.Tessellator tes, String... excluded) {
         throw new RuntimeException("Tessellate not supported on HFR model");
+    }
+
+    class VBOBufferData {
+        String name;
+        int vertices = 0;
+        int vboHandle;
+        int vaoHandle;
     }
 
 }
