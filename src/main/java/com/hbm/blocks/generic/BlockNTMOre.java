db6b5f678c6a80b8aae59f2307c41647c288508c
Leafia <121738096+ntmleafia@users.noreply.github.com>
2025-12-13 19:51:14 +0900

    fix fucking BlockNTMOre

--- src/main/java/com/hbm/blocks/generic/BlockNTMOre.java
+++ src/main/java/com/hbm/blocks/generic/BlockNTMOre.java
@@ -18,10 +18,12 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import com.hbm.util.I18nUtil;
 
 import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Random;
+import java.util.Arrays;
 
 import static com.hbm.blocks.OreEnumUtil.OreEnum;
 
